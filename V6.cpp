709db785932307e781b8ef9785a17f28d275d7a6
YousefAbdalla <92184982+yousefabdalla28@users.noreply.github.com>
2024-05-10 18:01:27 +0300

    Update V6.cpp

--- V6.cpp
+++ V6.cpp
@@ -4,44 +4,74 @@ you should initalize the array from 1 to N
 Add specific value B to Matrix values
 YOUR CODE SHOULD RUN AT ANY NUM OF PROCESSORS
 */
-#include <stdio.h>
-#include<iostream>
+
+#include <iostream>
 #include <mpi.h>
+
 using namespace std;
-int main() {
+int main()
+{
+	// intialize MPI
 	MPI_Init(NULL, NULL);
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int rank,size;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int N, B;
-	if (rank == 0) {
-		cout << "Please enter the size of array N: " << endl;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int B, N;
+	if (rank == 0)
+	{
+		cout << "Enter the size of the array: ";
 		cin >> N;
-		cout << "Please enter the values to be added B:" << endl;	
+		cout << "Enter the value of b: ";
 		cin >> B;
 	}
-	MPI_Bcast(&N,1,MPI_INT,0,MPI_COMM_WORLD);
+
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&B, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	int* arr = new int[N];
-	int* localArr = new int[N / size];
-	if (rank == 0) {
-		for (int i = 0; i < N; i++) {
-			arr[i] = i + 1;
+
+	int* A = new int[N];
+
+	if (rank == 0)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			A[i] = (i + 1);
 		}
-		cout << "data generated at rank 0" << endl;
 	}
-	MPI_Scatter(arr, N / size, MPI_INT, localArr, N / size, MPI_INT, 0, MPI_COMM_WORLD);
-	for (int i = 0; i < N / size; i++) {
-		localArr[i] += B;
+
+	int length = N / size;
+
+	int* local_A = new int[length];
+	MPI_Scatter(A, length, MPI_INT, local_A, length, MPI_INT, 0, MPI_COMM_WORLD);
+
+	for (int i = 0; i < length; i++)
+	{
+		local_A[i] += B;
 	}
 	int* result = new int[N];
-	MPI_Gather(localArr, N / size, MPI_INT, result, N / size, MPI_INT,0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		cout << "the matrix result is " << endl;
-		for (int i = 0; i < N; i++) {
-			cout << result[i] << endl;
+
+	MPI_Gather(local_A, length, MPI_INT, result, length, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank == 0)
+	{
+
+		// handling the remainders
+		int remainder = N - (N % size);
+
+		for (int i = remainder; i < N; i++)
+		{
+			result[i] = A[i] + B;
 		}
+		cout << "The result is: ";
+
+		for (int i = 0; i < N; i++)
+		{
+			cout << result[i] << " ";
+		}
+
+		cout << endl;
 	}
+
 	MPI_Finalize();
-	return 0;
 }
