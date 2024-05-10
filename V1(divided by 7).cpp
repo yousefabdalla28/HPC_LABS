6a4ac8bd6912f84f95acad146ef9e862ebd4b3ae
YousefAbdalla <92184982+yousefabdalla28@users.noreply.github.com>
2024-05-10 19:55:15 +0300

    Update V1(divided by 7).cpp

--- V1(divided by 7).cpp
+++ V1(divided by 7).cpp
@@ -3,37 +3,62 @@
 // array declaration in the master rank
 // at least 3 ranks parallel
 // result printed via master rank
-#include <stdio.h>
-#include<iostream>
+
+#include <iostream>
 #include <mpi.h>
 using namespace std;
-int main() {
-	MPI_Init(NULL, NULL);
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int arr[150];
-	if (rank == 0) {
-		for (int i = 0; i < 150; i++) {
-			arr[i] = i + 1;
-		}
-		cout << "data generated at rank 0" << endl;
-	}
-	int processors[3] = { 0,0,0 };
-	int localCount = 0;
-	int localArr[50];
-	int totalCount = 0;
-	MPI_Scatter(&arr,50,MPI_INT,&localArr,50,MPI_INT,0,MPI_COMM_WORLD);
-	for (int i = 0; i < 50; i++) {
-		if (localArr[i] % 7 == 0) {
-			localCount++;
-		}
-	}
-	MPI_Reduce(&localCount,&totalCount,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-	if (rank == 0) {
-		cout << "The count of numbers divided by 7 is : " << totalCount;
-	}
-	MPI_Finalize();
 
-	return 0;
+
+int main()
+{
+    MPI_Init(NULL, NULL);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int a[150];
+
+    if (rank == 0)
+    {
+
+        for (int i = 0; i < 150; i++)
+        {
+            a[i] = i + 1;
+        }
+    }
+
+    int length = 150 / size;
+    int* localarr = new int[length];
+
+    MPI_Scatter(&a, length, MPI_INT, localarr, length, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (localarr[i] % 7 == 0)
+        {
+            count++;
+        }
+    }
+
+    int result;
+    MPI_Reduce(&count, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+
+        // handle the remaining elements
+        int reminder = 150 - (150 % size);
+        for (int i = reminder; i < 150; i++)
+        {
+            if (a[i] % 7 == 0)
+            {
+                result++;
+            }
+        }
+        cout << "Total numbers divisible by 7: " << result << endl;
+    }
+
+    MPI_Finalize();
 }
