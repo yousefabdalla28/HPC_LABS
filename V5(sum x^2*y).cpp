9ffe3512b0ca503d752d2708fa1d20876bdc0984
YousefAbdalla <92184982+yousefabdalla28@users.noreply.github.com>
2024-05-11 14:17:31 +0300

    Update V5(sum x^2*y).cpp

--- V5(sum x^2*y).cpp
+++ V5(sum x^2*y).cpp
@@ -14,26 +14,29 @@ int main()
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int  localSum = 0, result;
     int x[50];
     int y[50];
-    int localX[10];
-    int localY[10];
     if (rank == 0) {
-        for (int i = 0; i < 50; i++) { 
+        for (int i = 0; i < 50; i++) {
             x[i] = i + 1;
             y[i] = i + 1;
-        }  
+        }
     }
-    MPI_Scatter(&x,50/size,MPI_INT,localX, 50 / size,MPI_INT,0,MPI_COMM_WORLD);
+    int length = 50 / size;
+    int* localX = new int[length];
+    int* localY = new int[length];
+    MPI_Scatter(&x, 50 / size, MPI_INT, localX, 50 / size, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Scatter(&y, 50 / size, MPI_INT, localY, 50 / size, MPI_INT, 0, MPI_COMM_WORLD);
+    int  localSum = 0, result;
     for (int i = 0; i < 50 / size; i++) {
         localSum += (localX[i] * localX[i]) * localY[i];
     }
     MPI_Reduce(&localSum, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
     if (rank == 0) {
-        for (int i = (50 / size) * size; i < 50; i++) {
-            result += (x[i]*y[i])*y[i];
+        int reminder = 50 - (50 % size);
+        for (int i =reminder; i < 50; i++) {
+            result += (x[i] * x[i]) * y[i];
         }
         cout << "Result is = " << result << endl;
     }
